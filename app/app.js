04b805793f893b09736e0539941a3e21e89a6080
Hugo Mota <hugo.txt@gmail.com>
2016-06-19 15:15:15 -0300

    Add support to Sass and improve webpack configuration
    Now it has two different configurations: dev and build. The build
    configuration will skip source maps and emmit actual files for
    styles, with hashes in their names.

--- app/app.js
+++ app/app.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './style.scss';
 
 
 class App extends React.Component
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,57 @@
-module.exports =
-{
+var build = Boolean(process.env.BUILD);
+var dev = !build;
+
+
+console.log('-------------------------------------------');
+console.log(dev ? 'DEV MODE' : 'BUILD MODE');
+console.log('-------------------------------------------');
+
+
+var config = {
     entry: './app/index',
-    devtool: 'source-map',
-    output: {
-        path: 'static',
-        filename: 'bundle.js',
-    },
     module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                loader: 'react-hot',
-                include: __dirname + '/app',
-            },
-            {
-                test: /\.js$/,
-                include: __dirname + '/app',
-                loader: 'babel',
-                query: {
-                    presets: ['es2015', 'react']
-                },
-            }
-        ],
-    },
+        loaders: []
+    }
 };
+
+
+var loaders = {
+    hot: {
+        test: /\.js$/,
+        loader: 'react-hot',
+        include: __dirname + '/app',
+    },
+    babel: {
+        test: /\.js$/,
+        include: __dirname + '/app',
+        loader: 'babel',
+        query: {
+            presets: ['es2015', 'react']
+        },
+    },
+    stylesDev: {
+        test: /\.s?css$/,
+        loaders: ['style', 'css', 'sass'],
+        include: __dirname + '/app',
+    },
+    stylesBuild: {
+        test: /\.s?css$/,
+        loaders: ['style/url', 'file?name=[hash].css', 'extract', 'css', 'sass'],
+        include: __dirname + '/app',
+    }
+};
+
+
+if (dev) {
+    config['output'] = {path: 'static', filename: 'bundle.js'};
+    config['module']['loaders'] = [loaders.hot, loaders.babel, loaders.stylesDev];
+    config['devtool'] = 'source-map';
+}
+
+if (build) {
+    config['output'] = {path: 'build', filename: 'bundle.js'};
+    config['module']['loaders'] = [loaders.hot, loaders.babel, loaders.stylesBuild];
+}
+
+
+module.exports = config;
