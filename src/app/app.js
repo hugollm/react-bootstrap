0f20d4b11eec03ffbc126d688c0cb2f9e1e68da2
Hugo Mota <hugo.txt@gmail.com>
2017-10-29 10:01:06 -0200

    Move app into a subdirectory of src

--- src/app/app.js
+++ src/app/app.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import './style.scss';
+import './app.scss';
 
 
-export class App extends React.Component {
+export default class App extends React.Component {
 
     render() {
         return <h1>Hello World</h1>;
